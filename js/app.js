2fb34a5b7a62f07fcd7b380e1bda009125c491a5
Rebeca Assis <rebeca.oassis@hotmail.com>
2021-05-20 15:47:14 -0300

    Refatorei o código.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,3 @@
-const preencheCampos = json => {
-    document.querySelector('input[name=street]').value = json.logradouro;
-    document.querySelector('input[name=district]').value = json.bairro;
-    document.querySelector('input[name=complement]').value = json.complemento;
-    document.querySelector('input[name=city]').value = json.localidade;
-    document.querySelector('input[name=state]').value = json.uf;
-}
-
-const buscaCep = cep => {
-    let url = 'http://viacep.com.br/ws/' + cep + '/json';
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-            if (xhr.status = 200) {
-                preencheCampos(JSON.parse(xhr.responseText));
-            }
-        }
-    }
-    xhr.send();
-}
-
 let cepNumebers = [];
 
 document.querySelector("#cep")
@@ -32,3 +10,27 @@ document.querySelector("#cep")
 
         console.log(e);
     })
+
+    const buscaCep = cep => {
+        let url = 'http://viacep.com.br/ws/' + cep + '/json';
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status = 200) {
+                    preencheCampos(JSON.parse(xhr.responseText));
+                }
+            }
+        }
+        xhr.send();
+    }
+
+const preencheCampos = json => {
+    document.querySelector('input[name=street]').value = json.logradouro;
+    document.querySelector('input[name=district]').value = json.bairro;
+    document.querySelector('input[name=complement]').value = json.complemento;
+    document.querySelector('input[name=city]').value = json.localidade;
+    document.querySelector('input[name=state]').value = json.uf;
+}
+
+
